947841a45824ff258a21f76e877566bef08c62dd
Pedro Morelli <pedromorelli96@gmail.com>
2022-09-11 18:13:56 -0300

    [module 3 / unit-converter] Adds unit converter solo project

---
+++ module 3/unit-converter/index.js
@@ -0,0 +1,18 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const numberInput = document.getElementById("number-input")
+const convertBtn = document.getElementById("convert-btn")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+
+convertBtn.addEventListener("click", function() {
+    const value = Number(numberInput.value)
+    lengthEl.textContent = `${value} meters = ${(value * 3.281).toFixed(3)} feet | ${value} feet = ${(value / 3.281).toFixed(3)} meters`
+    volumeEl.textContent = `${value} liters = ${(value * 0.264).toFixed(3)} gallons | ${value} gallons = ${(value / 0.264).toFixed(3)} liters`
+    massEl.textContent = `${value} kilos = ${(value * 2.204).toFixed(3)} pounds | ${value} pounds = ${(value / 2.204).toFixed(3)} kilos`
+})
