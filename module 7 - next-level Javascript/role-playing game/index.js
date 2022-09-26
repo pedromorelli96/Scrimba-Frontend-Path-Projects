d529bf316f26be09b7ccc9b07c97b1e1c328f728
Pedro Morelli <pedromorelli96@gmail.com>
2022-09-26 20:42:53 -0300

    [module 7 / role-playing game] Updates to role-playing game project

--- module 7 - next-level Javascript/role-playing game/index.js
+++ module 7 - next-level Javascript/role-playing game/index.js
@@ -1,45 +1,11 @@
-function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function(){
-        return Math.floor(Math.random() * 6) + 1
-    })    
+import characterData from '/data.js'
+import Character from '/Character.js'
+
+function render() {
+    document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
+    document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
 }
 
-function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount).map(function(num){ 
-        return  `<div class="dice">${num}</div>`
-    }).join('')
-}
-
-const hero = {
-    elementId: "hero",
-    name: "Wizard",
-    avatar: "images/wizard.png",
-    health: 60,
-    diceCount: 3
-}
-
-const monster = {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "images/orc.png",
-    health: 10,
-    diceCount: 1
-}
-
-function renderCharacter(data) {
-    const { elementId, name, avatar, health, diceCount } = data;
-    const diceHtml = getDiceHtml(diceCount)
-
-    document.getElementById(elementId).innerHTML =
-        `<div class="character-card">
-            <h4 class="name"> ${name} </h4>
-            <img class="avatar" src="${avatar}" />
-            <div class="health">health: <b> ${health} </b></div>
-            <div class="dice-container">    
-                ${diceHtml}
-            </div>
-        </div>`;
-}
-
-renderCharacter(hero);
-renderCharacter(monster);
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
+render()
