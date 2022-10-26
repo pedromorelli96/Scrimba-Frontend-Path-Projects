71c47b49ce4e0a3f11ae7f3644da753fc977b989
Pedro Morelli <pedromorelli96@gmail.com>
2022-10-25 22:34:08 -0300

    [module 9 / Movie Watchlist] Updates to HTML structure and basic styling

--- module 9 - working with APIs/Movie Watchlist/index.js
+++ module 9 - working with APIs/Movie Watchlist/index.js
@@ -32,4 +32,45 @@ async function getMovieInfo(movieIds) {
     }
 }
 
-getMovieInfo(getMovies("Blade Runner"));
+// getMovieInfo(getMovies("Blade Runner"));
+
+// {
+//     "Title":"Blade Runner",
+//     "Year":"1982",
+//     "Rated":"R",
+//     "Released":"25 Jun 1982",
+//     "Runtime":"117 min",
+//     "Genre":"Action, Drama, Sci-Fi",
+//     "Director":"Ridley Scott",
+//     "Writer":"Hampton Fancher, David Webb Peoples, Philip K. Dick",
+//     "Actors":"Harrison Ford, Rutger Hauer, Sean Young",
+//     "Plot":"A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.",
+//     "Language":"English, German, Cantonese, Japanese, Hungarian, Arabic, Korean",
+//     "Country":"United States",
+//     "Awards":"Nominated for 2 Oscars. 12 wins & 19 nominations total",
+//     "Poster":"https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+//     "Ratings":[
+//        {
+//           "Source":"Internet Movie Database",
+//           "Value":"8.1/10"
+//        },
+//        {
+//           "Source":"Rotten Tomatoes",
+//           "Value":"89%"
+//        },
+//        {
+//           "Source":"Metacritic",
+//           "Value":"84/100"
+//        }
+//     ],
+//     "Metascore":"84",
+//     "imdbRating":"8.1",
+//     "imdbVotes":"762,768",
+//     "imdbID":"tt0083658",
+//     "Type":"movie",
+//     "DVD":"30 Oct 2001",
+//     "BoxOffice":"$32,914,489",
+//     "Production":"N/A",
+//     "Website":"N/A",
+//     "Response":"True"
+//  }
