6b0dffb811d644ae9ee491dcd683d12b954e0ce6
Pedro Morelli <pedromorelli96@gmail.com>
2022-11-08 19:03:58 -0300

    [module 9 / Movie Watchlist] Adds feedback toast and updates localStorage logic

--- module 9 - working with APIs/Movie Watchlist/index.js
+++ module 9 - working with APIs/Movie Watchlist/index.js
@@ -2,13 +2,13 @@ const apiKey = "202089d";
 let moviesObjectArray = [];
 let watchlist = [];
 const watchlistFromLocalStorage = JSON.parse(localStorage.getItem("watchlist"));
-console.log("firstLoad watchlistFromLocalStorage: ", watchlistFromLocalStorage);
 
 const searchInput = document.getElementById("search-input");
 const moviesList = document.getElementById("movies-list");
 const emptyMovies = document.getElementById("empty-movies");
 const emptySearch = document.getElementById("empty-search");
 const moviesSynopsis = document.querySelectorAll(".movie-synopsis");
+const toast = document.getElementById("toast");
 
 function truncateMoviesSynopsis(synopsis) {
     if (synopsis.length > 150) {
@@ -175,6 +175,22 @@ function renderMovieList() {
     moviesList.innerHTML = moviesHmtl;
 }
 
+function showToast(action, movieTitle) {
+    let toastMessage = ``;
+    if (action === "add") {
+        toastMessage = `${movieTitle} added to watchlist`;
+    } else {
+        toastMessage = `${movieTitle} removed from watchlist`;
+    }
+    toast.textContent = toastMessage;
+
+    toast.className = "show";
+
+    setTimeout(function () {
+        toast.className = toast.className.replace("show", "");
+    }, 3000);
+}
+
 if (watchlistFromLocalStorage) {
     watchlist = watchlistFromLocalStorage;
 }
@@ -189,19 +205,33 @@ document.addEventListener("submit", function (e) {
 
 moviesList.addEventListener("click", function (e) {
     if (e.target.dataset.movieid) {
+        // Use dataset id to find movie object
         const index = moviesObjectArray.findIndex(
             (movie) => movie.id === e.target.dataset.movieid
         );
 
+        // Decide to add to/remove from movie watchlist
+        // Call add/remove toast based on inWatchlist boolean
+        if (moviesObjectArray[index].inWatchlist) {
+            watchlist = watchlist.filter(
+                (movie) => movie.id !== moviesObjectArray[index].id
+            );
+            showToast("remove", moviesObjectArray[index].title);
+        } else {
+            watchlist.push(moviesObjectArray[index]);
+            showToast("add", moviesObjectArray[index].title);
+        }
+
+        // Update inWatchlist boolean to be its oposite
         moviesObjectArray[index].inWatchlist =
             !moviesObjectArray[index].inWatchlist;
 
-        watchlist.push(moviesObjectArray[index]);
-
-        localStorage.setItem("watchlist", JSON.stringify(watchlist));
-
-        // add confirmation toast here
-        // https://www.w3schools.com/howto/howto_js_snackbar.asp
+        // Update localStorage
+        if (watchlist.length === 0) {
+            localStorage.clear();
+        } else {
+            localStorage.setItem("watchlist", JSON.stringify(watchlist));
+        }
 
         renderMovieList();
     }
