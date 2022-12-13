508ff41ec3525f3ddd797c1230b1a06d8fac1085
Pedro Morelli <pedromorelli96@gmail.com>
2022-12-12 22:01:23 -0300

    [module 3 / unit converter] Minor updates to Unit Converter Solo Project

--- module 3 - interactive websites/unit-converter/index.js
+++ module 3 - interactive websites/unit-converter/index.js
@@ -4,15 +4,18 @@
 1 kilogram = 2.204 pound
 */
 
-const numberInput = document.getElementById("number-input")
-const convertBtn = document.getElementById("convert-btn")
-const lengthEl = document.getElementById("length-el")
-const volumeEl = document.getElementById("volume-el")
-const massEl = document.getElementById("mass-el")
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const lengthEl = document.getElementById("length-el");
+const volumeEl = document.getElementById("volume-el");
+const massEl = document.getElementById("mass-el");
 
-convertBtn.addEventListener("click", function() {
-    const value = Number(numberInput.value)
+convertBtn.addEventListener("click", function () {
+    const value = Number(numberInput.value);
+    // prettier-ignore
     lengthEl.textContent = `${value} meters = ${(value * 3.281).toFixed(3)} feet | ${value} feet = ${(value / 3.281).toFixed(3)} meters`
+    // prettier-ignore
     volumeEl.textContent = `${value} liters = ${(value * 0.264).toFixed(3)} gallons | ${value} gallons = ${(value / 0.264).toFixed(3)} liters`
+    // prettier-ignore
     massEl.textContent = `${value} kilos = ${(value * 2.204).toFixed(3)} pounds | ${value} pounds = ${(value / 2.204).toFixed(3)} kilos`
-})
+});
