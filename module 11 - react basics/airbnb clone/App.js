2688734432d4dc6fa68a90a7e3f11331bf598d63
Pedro Morelli <pedromorelli96@gmail.com>
2022-12-22 21:06:12 -0300

    [module 11 / airbnb clone] Final updates to AirBnb clone project

--- module 11 - react basics/airbnb clone/App.js
+++ module 11 - react basics/airbnb clone/App.js
@@ -2,20 +2,26 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 
 export default function App() {
-            // <Hero />
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+                
+            />
+        )
+    })        
+    
     return (
         <div>
             <Navbar />
-            <Card 
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
     )
 }
