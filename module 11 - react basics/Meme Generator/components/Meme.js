27a27a9a70b082a82e05121155cef8b0a30ccab7
Pedro Morelli <pedromorelli96@gmail.com>
2022-12-25 21:08:40 -0300

    [module 11 / meme generator] Adds initial files of Meme Generator project

---
+++ module 11 - react basics/Meme Generator/components/Meme.js
@@ -0,0 +1,44 @@
+import React from "react"
+import memesData from "../memesData.js"
+
+export default function Meme() {
+    /**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     * 
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+     */
+    let url
+    
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        url = memesArray[randomNumber].url
+        console.log(url)
+    }
+    
+    return (
+        <main>
+            <p>{url}</p>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+        </main>
+    )
+}
