257e6609e7d4475298da828fde6675333d293bc2
Pedro Morelli <pedromorelli96@gmail.com>
2022-12-26 22:44:59 -0300

    [module 11 / meme generator] State updates to Meme Generator project

--- module 11 - react basics/Meme Generator/components/Meme.js
+++ module 11 - react basics/Meme Generator/components/Meme.js
@@ -3,24 +3,44 @@ import memesData from "../memesData.js"
 
 export default function Meme() {
     /**
-     * Challenge: Get a random image from the `memesData` array
-     * when the "new meme image" button is clicked.
+     * Challenge: Update our state to save the meme-related
+     * data as an object called `meme`. It should have the
+     * following 3 properties:
+     * topText, bottomText, randomImage.
      * 
-     * Log the URL of the image to the console. (Don't worry
-     * about displaying the image yet)
+     * The 2 text states can default to empty strings for now,
+     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * 
+     * Next, create a new state variable called `allMemeImages`
+     * which will default to `memesData`, which we imported above
+     * 
+     * Lastly, update the `getMemeImage` function and the markup 
+     * to reflect our newly reformed state object and array in the
+     * correct way.
      */
-    let url
+    
+    // const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
     
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        url = memesArray[randomNumber].url
-        console.log(url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
     }
     
     return (
         <main>
-            <p>{url}</p>
             <div className="form">
                 <input 
                     type="text"
@@ -39,6 +59,7 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
+            <img src={meme.randomImage} className="meme--image" />
         </main>
     )
 }
