4f4b0dadc16783a34f792e89b114e56d67cd349a
Pedro Morelli <pedromorelli96@gmail.com>
2022-12-30 13:34:03 -0300

    [module 11 / react info] Adds dark/light mode toggle to React Info site project

--- module 11 - react basics/react info/App.js
+++ module 11 - react basics/react info/App.js
@@ -1,12 +1,21 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Main from "./components/Main";
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
 export default function App() {
+    const [darkMode, setDarkMode] = React.useState(true)
+    
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+    
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar 
+                darkMode={darkMode} 
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode={darkMode} />
         </div>
-    );
+    )
 }
