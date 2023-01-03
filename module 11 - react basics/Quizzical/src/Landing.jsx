d637eb68328d05a27c48f616239afc840200def2
Pedro Morelli <pedromorelli96@gmail.com>
2023-01-03 16:21:10 -0300

    [module 11 / quizzical] Minor update to components

---
+++ module 11 - react basics/Quizzical/src/Landing.jsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+    return (
+        <div>
+            <h1>Landing Component</h1>
+        </div>
+    );
+}
---
+++ module 11 - react basics/Quizzical/src/Question.jsx
@@ -0,0 +1,7 @@
+export default function Question() {
+    return (
+        <div>
+            <h1>Question Component</h1>
+        </div>
+    );
+}
