e3ccc206f874339c21a30f2ef9fa89b27305fb7c
Pedro Morelli <pedromorelli96@gmail.com>
2023-01-04 22:18:17 -0300

    [module 11 / quizzical] Minor update to components

--- module 11 - react basics/Quizzical/src/App.jsx
+++ module 11 - react basics/Quizzical/src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
-        <div className="container">
+        <main className="container">
             <h1>App Component</h1>
-        </div>
+        </main>
     );
 }
