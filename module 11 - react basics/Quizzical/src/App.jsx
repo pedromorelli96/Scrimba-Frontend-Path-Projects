2baf6be5a32ad255db327bdeb28c0d770337b1db
Pedro Morelli <pedromorelli96@gmail.com>
2023-01-05 21:40:12 -0300

    [module 11 / quizzical] Adds data fetching from Open Trivia DB API and landing page components

--- module 11 - react basics/Quizzical/src/App.jsx
+++ module 11 - react basics/Quizzical/src/App.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import Quiz from "./Quiz";
+
 export default function App() {
+    const [quiz, setQuiz] = useState([]);
+    const [questionAmount, setQuestionAmount] = useState(5);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleFetchQuiz = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await fetch(
+                `https://opentdb.com/api.php?amount=${questionAmount}`
+            );
+            const data = await response.json();
+            setQuiz(data.results);
+            console.log(data.results);
+        } catch (error) {
+            console.log("(!) Error: ", error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     return (
-        <main className="container">
-            <h1>App Component</h1>
+        <main>
+            {quiz.length > 0 ? (
+                <Quiz />
+            ) : (
+                <div className="landing-container">
+                    {isLoading ? (
+                        <h1 className="loading-text">Loading your quiz...</h1>
+                    ) : (
+                        <>
+                            <h1 className="landing-title">Quizzical</h1>
+                            <p className="landing-description">Test your knowledge!</p>
+                            <button className="start-quiz-btn" onClick={handleFetchQuiz}>Start quiz</button>
+                        </>
+                    )}
+                </div>
+            )}
         </main>
     );
 }
---
+++ module 11 - react basics/Quizzical/src/Quiz.jsx
@@ -0,0 +1,9 @@
+import Question from "./Question";
+
+export default function Quiz() {
+    return (
+        <div>
+            <h1>Quiz Component</h1>
+        </div>
+    );
+}
